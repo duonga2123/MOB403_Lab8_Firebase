87aa55d4b13841987f2f10279bfa12f2ab721e6c
Duong <thanhduonga2123@gmail.com>
2023-08-04 07:59:09 +0700

    lab8

--- app/src/main/java/com/example/testfirebase/Bai2/LoginSignUpFragment.java
+++ app/src/main/java/com/example/testfirebase/Bai2/LoginSignUpFragment.java
@@ -33,7 +33,7 @@ public class LoginSignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home, container, false);
+        return inflater.inflate(R.layout.login_signup, container, false);
     }
 
     public static LoginSignUpFragment newInstance() {
